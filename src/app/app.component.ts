2569118de63fcad170ad85b6bc534d7b3d964268
khushi2023 <khushijaiswal2023@gmail.com>
2024-04-11 22:58:31 +0530

    Created left and right continer-Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LeftContainerComponent } from './left-container/left-container.component';
+import { RightContainerComponent } from './right-container/right-container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LeftContainerComponent, RightContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
